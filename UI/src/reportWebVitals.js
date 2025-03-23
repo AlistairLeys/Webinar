b4586ddab9ca38ae4a75e9b82b1e871c2c1015af
AlistairLeys <alistairleys@gmail.com>
2025-03-23 22:24:11 +0000

    modified:   UI/package-lock.json 	modified:   UI/package.json 	modified:   UI/src/reportWebVitals.js 	modified:   UI/src/reportWebVitals.tsx

--- UI/src/reportWebVitals.js
+++ UI/src/reportWebVitals.js
@@ -1,11 +1,11 @@
 const reportWebVitals = (onPerfEntry) => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
-        import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-            getCLS(onPerfEntry);
-            getFID(onPerfEntry);
-            getFCP(onPerfEntry);
-            getLCP(onPerfEntry);
-            getTTFB(onPerfEntry);
+        import('web-vitals').then((vitals) => {
+            vitals.onCLS(onPerfEntry);
+            vitals.onFID(onPerfEntry);
+            vitals.onFCP(onPerfEntry);
+            vitals.onLCP(onPerfEntry);
+            vitals.onTTFB(onPerfEntry);
         });
     }
 };
